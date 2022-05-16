4e32beafdc459217ebc8c1278693f1ea988caa34
moontai0724 <moontai0724@gmail.com>
2022-05-16 13:34:10 +0800

    chore: setup project

---
+++ HEADER.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         巴哈列表過濾器
+// @namespace    https://home.gamer.com.tw/moontai0724
+// @version      5.0.0
+// @description  在巴哈姆特透過「黑名單、關鍵字、字數限制」過濾文章、留言，在頂端列可以開關過濾器（一次性）
+// @author       moontai0724
+// @match        https://forum.gamer.com.tw/B.php*
+// @match        https://forum.gamer.com.tw/Bo.php*
+// @match        https://forum.gamer.com.tw/C.php*
+// @match        https://forum.gamer.com.tw/Co.php*
+// @match        https://forum.gamer.com.tw/search.php*
+// @grant        none
+// @supportURL   https://github.com/moontai0724/bahamut-forum-filter-script/issues
+// @license      MIT
+// ==/UserScript==
