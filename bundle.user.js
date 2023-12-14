cd931e175735208c970a8d1f5c5b84aa90ecb6f6
moontai0724 <moontai0724@gmail.com>
2023-12-14 01:58:25 +0000

    Update release to output generated at 133c5da

---
+++ bundle.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name            Your Script Name
+// @namespace       https://example.com/
+// @match           *://*/*
+// @version         0.0.0
+// @description     A brief description for your script.
+// @author          moontai0724
+// @homepage        https://github.com/moontai0724/
+// @supportURL      https://github.com/moontai0724/userscript-template
+// ==/UserScript==
+
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+/******/ })()
+;
